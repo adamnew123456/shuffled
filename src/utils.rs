4c048c46c1a40241526d74c904c0184c9fd529df
adamnew123456 <adamnew123456@gmail.com>
2020-11-03 21:22:18 -0500

    Integrate support for writing ID3 directly to avoid eyeD3 dependency

--- src/utils.rs
+++ src/utils.rs
@@ -46,15 +46,29 @@ pub fn read_text_announcement(
         .output()
         .or_else(|err| Err(format!("Could not invoke lame: {}", err)))?;
 
-    Command::new("/usr/bin/eyeD3")
-        .arg("-1")
-        .arg("-a")
-        .arg("shuffled")
-        .arg("-t")
-        .arg(title)
-        .arg(outputs.lame_mp3)
-        .output()
-        .or_else(|err| Err(format!("Could not invoke eyeD3: {}", err)))?;
+    // ID3v1 header
+    let mut mp3_options = fs::OpenOptions::new();
+    let mut mp3 = mp3_options
+        .append(true)
+        .open(outputs.lame_mp3)
+        .or_else(|err| Err(format!("Could not open MP3 file for write: {}", err)))?;
+
+    let empty_field = [0; 30];
+
+    let mut title_ext = title.as_bytes().to_vec();
+    title_ext.resize(30, 0);
+
+    let artist_raw = "shuffled";
+    let mut artist_ext = artist_raw.as_bytes().to_vec();
+    artist_ext.resize(30, 0);
+
+    mp3.write("TAG".as_bytes()).or_else(|err| Err(format!("Could not write ID3: {}", err)))?;
+    mp3.write(&title_ext).or_else(|err| Err(format!("Could not write ID3: {}", err)))?;
+    mp3.write(&artist_ext).or_else(|err| Err(format!("Could not write ID3: {}", err)))?;
+    mp3.write(&empty_field).or_else(|err| Err(format!("Could not write ID3: {}", err)))?; // Album
+    mp3.write("2020".as_bytes()).or_else(|err| Err(format!("Could not write ID3: {}", err)))?; // year
+    mp3.write(&empty_field).or_else(|err| Err(format!("Could not write ID3: {}", err)))?; // Comment
+    mp3.write(&[28]).or_else(|err| Err(format!("Could not write ID3: {}", err)))?; // Vocal genre
 
     Ok(())
 }
